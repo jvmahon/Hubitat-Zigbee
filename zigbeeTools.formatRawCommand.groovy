0d14598ac3034720843da1b747f140456f389f3c
jvmahon <jvm33@columbia.edu>
2022-10-20 12:31:44 -0400

    Delete zigbeeTools.formatRawCommand.groovy

--- zigbeeTools.formatRawCommand.groovy
+++
@@ -1,8 +0,0 @@
-
-
-String formatZCLHeader(Map inputs = [frameType: null , mfrSpecific: true , direction: 0, disableDefaultResponse: false]) {
-	assert inputs.frameType instanceof Integer
-	
-	Integer returnValue = 0
-	returnValue = returnValue
-}
